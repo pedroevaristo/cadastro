a552c5da55b6877af3e85b657d32ed72c6b63539
pedroevaristo <hattoriminamoto@gmail.com>
2023-10-13 21:34:03 -0300

    13/10/2023

--- cadastroCliente.java
+++ cadastroCliente.java
@@ -24,21 +24,63 @@ public class cadastroCliente {
         Map<Integer, Pessoa> listaPessoa = new HashMap<>();
 
         System.out.println("Quantidades de clientes:");
-        Integer clients = sc.nextInt();
+        Integer clientes = sc.nextInt();
 
         System.out.println("Digite as seguintes informações");
-        for(int i = 0; i < clients; i++) {
+        inserirInfo(clientes);
+
+        System.out.printf("Digite entre 0 e %d", clientes);
+        String type = "";
+        boolean status = true;
+        while(status) {
+
+            int numeroListaPessoa = sc.nextInt();
+            sc.nextLine();
+
+            Pessoa consulta = listaPessoa.get(numeroListaPessoa);
+
+            if(consulta != null){
+                System.out.println("Cliente" + numeroListaPessoa +": "+ consulta);
+                System.out.println("Quer colocar mais alguma informações nova?");
+                String escolha = sc.next();
+
+                if(escolha.equals("sim")){
+
+                } else if (escolha.equals("nao") || escolha.equals("n")) {
+
+                }
+            }else{
+                System.out.println("Não foi encontrado Quer colocar a informação como novo cadastro?");
+                 type = sc.nextLine();
+            }
+
+        }
+        sc.close();
+    }
+
+    private static boolean verificarCEP(String cepPerson) {
+        if (cepPerson.length() == 8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private static void inserirInfo(int clientes){
+        Scanner sc = new Scanner(System.in);
+        Map<Integer, Pessoa> listaPessoa = new HashMap<>();
+
+        for(int i = 0; i < clientes; i++) {
             System.out.println("Nome");
-                String namePerson = sc.next();
+            String namePerson = sc.next();
             System.out.println("Sobrenome");
-                String surnamePerson = sc.next();
+            String surnamePerson = sc.next();
             System.out.println("CEP");
-                String cepPerson = sc.next();
+            String cepPerson = sc.next();
             System.out.println("número de telefone");
-                String cellPhonePerson = sc.next();
+            String cellPhonePerson = sc.next();
 
 
-            if (verifyCEP(cepPerson) == true) {
+            if (verificarCEP(cepPerson) == true) {
                 System.out.println("verificando...achou");
                 Pessoa pessoa = new Pessoa(namePerson, surnamePerson,cepPerson,cellPhonePerson);
                 listaPessoa.put(i, pessoa);
@@ -57,37 +99,6 @@ public class cadastroCliente {
 
                 }
             }
-        }
-        System.out.printf("Digite entre 0 e %d", clients);
-        String type = "";
-        boolean status = true;
-        while(status) {
-
-            int numeroListaPessoa = sc.nextInt();
-            sc.nextLine();
-
-            Pessoa consulta = listaPessoa.get(numeroListaPessoa);
-
-            if(consulta != null){
-                System.out.println("Cliente" + numeroListaPessoa +": "+ consulta);
-                System.out.println("Quer colocar mais alguma informações nova?");
-                if(){
-                    break;
-                }
-            }else{
-                System.out.println("Não foi encontrado Quer colocar a informação como novo cadastro?");
-                 type = sc.nextLine();
-            }
-
-        }
-        sc.close();
-    }
-
-    private static boolean verifyCEP(String cepPerson) {
-        if (cepPerson.length() == 8) {
-            return true;
-        } else {
-            return false;
         }
     }
 }
